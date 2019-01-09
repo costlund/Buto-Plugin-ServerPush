b50e6ee574d42108ab72b01de206e5c64781a4ce
Claes Ö <>
2019-01-09 10:50:39 +0100

    Add files.

---
+++ PluginServerPush.php
@@ -0,0 +1,15 @@
+<?php
+class PluginServerPush{
+  public function push($url, $data){
+    $postdata = http_build_query($data);
+    $opts = array('http' => 
+      array (
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $postdata
+      )
+    );
+    $context  = stream_context_create($opts);
+    return file_get_contents($url, false, $context);
+  }
+}
